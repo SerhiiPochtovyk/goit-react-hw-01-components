72d7709144ada014e84fe7589325442d022c3421
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-10-22 23:24:22 +0300

    create task04 Transactions;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import { Profile } from './Profile/Profile';
 import  Statistics  from './Statistics/Statistics';
 import data from '../assets/data.json';
 import  FriendList  from './FriendList/Friends';
-import friends from '../assets/friends.json'
+import friends from '../assets/friends.json';
+import  {TransactionHistory}  from './Transactions/transactions';
+import transactions from '../assets/transactions.json';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory
+        transactions={transactions} />;
     </div>
   );
 }
